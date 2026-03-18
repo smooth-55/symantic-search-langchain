2bdfba579c77641e7d7101ae56b730465c4f3d90
Ashish <dhakalashish18@gmail.com>
2026-03-18 22:24:33 +0545

    fix: something

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from typing import TypedDict, Annotated, Optional, Literal
 from constants import Datasets as documents, THRESHOLD, Transcript as user_transcript,PROMPT_TO_SPLIT_INTO_CHUNKS
 import os
 from dotenv import load_dotenv
+from pydantic import BaseModel, Field
 
 # Load variables from .env file
 load_dotenv()
@@ -19,38 +20,63 @@ class Transcript(TypedDict):
         "List of very short micro-sentences created by aggressively splitting the transcript. The list should be long."
     ]
 
+class Review(BaseModel):
+    review: Literal['positive', 'negative'] = Field(description='Give the sentiment of the Review')
+
 class SymenticSearch:
     def __init__(self):
         self.embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
-        self.chat_llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0.2)
+        self.chat_llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0)
         self.prompt = PromptTemplate(
-            template=f"{PROMPT_TO_SPLIT_INTO_CHUNKS} \n {user_transcript}",
+            template=f"{PROMPT_TO_SPLIT_INTO_CHUNKS} \n {{transcript}}",
             input_variables=["transcript"],
         )
+        self.reviewPrompt = PromptTemplate(
+            template=f"Check if the matches are correctly matched with correct intention into negative or positive. \n {{segment}} \n {{relevent_doc}}",
+            input_variables=["segment", "relevent_doc"],
+        )
+       
+    
 
     def main(self):
         vector_docs = self.embeddings.embed_documents(documents)
         structured_model = self.chat_llm.with_structured_output(Transcript)
-        chain = self.prompt | structured_model
+        structured_model2 = self.chat_llm.with_structured_output(Review)
+        chain = self.prompt | structured_model  | self.reviewPrompt | structured_model2
         result = chain.invoke({"transcript": user_transcript})
         segments = result["segments"]
         print(f"Total segments: {len(segments)}")
         # 4. Loop through each segment and search your database
         matched_task = set()
+        output = list()
+        okay = list()
         print(f"Total Task: {len(documents)}")
-        # for segment in segments:
-        #     # Embed the specific segment
-        #     segment_vector = embeddings.embed_query(segment)
-        #     # similiarity scores with every segements
-        #     similarity_scores = cosine_similarity([segment_vector], vector_docs)[0]
+        for segment in segments:
+            # Embed the specific segment
+            segment_vector = self.embeddings.embed_query(segment)
+            # similiarity scores with every segements
+            similarity_scores = cosine_similarity([segment_vector], vector_docs)[0]
 
-        #     for index, similarity_score in enumerate(similarity_scores):
-        #         if similarity_score >= THRESHOLD:
-        #             relevent_doc = documents[index]
-        #             if relevent_doc not in matched_task:
-        #                 matched_task.add(relevent_doc)
-        #                 print(f"   Score: {similarity_score:.2f}")
-        # print(f"Matched Task: {len(matched_task)}")
+            for index, similarity_score in enumerate(similarity_scores):
+                if similarity_score > THRESHOLD:
+                    relevent_doc = documents[index]
+                    if relevent_doc not in matched_task:
+                        matched_task.add(relevent_doc)
+                        # output.append({
+                        #     "matchedSegment": segment,
+                        #     "matchedWith": relevent_doc,
+                        #     "confidence": similarity_score,
+                        # })
+                        okay.append((
+                            segment,
+                            relevent_doc,
+                        ))
+                        print(f"   Score: {similarity_score:.2f}")
+        return okay
 
 
 obj = SymenticSearch().main()
+
+
+
+print(obj, len(obj))
